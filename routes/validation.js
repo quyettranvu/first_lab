e8dcb045940887431765f481637a34daee257222
quyettranvu <79063319+quyettranvu@users.noreply.github.com>
2022-03-17 03:05:53 +0700

    Update validation.js

--- routes/validation.js
+++ routes/validation.js
@@ -22,5 +22,5 @@ const loginValidation=(data)=>{
     return schema.validate(data);
 }
 
-module.exports.registerValidation=registerValidation;
+module.exports.registerValidation=registervalidation;
 module.exports.loginValidation=loginValidation;
